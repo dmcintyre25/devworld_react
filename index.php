d9dc901b32420ae039e1bd9b4a7845a87239d4ad
Daniel McIntyre <dmcintyre25@gmail.com>
2018-10-26 08:36:52 -0400

    adding wordpress loop

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 <h1>DevWorld</h1>
 
+
+<section>
+    <div>
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+        
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_excerpt(); ?></p>
+
+        <?php endwhile; else : ?>
+        
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
+        
+        <?php endif; ?>
+    </div>
+</section>
+
+
 <?php get_footer(); ?>

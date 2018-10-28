2acb2673a46e04a99543929b06104ed56dfaab7d
Daniel McIntyre <dmcintyre25@gmail.com>
2018-10-28 08:06:17 -0400

    Working on feature/child section

--- functions.php
+++ functions.php
@@ -11,6 +11,12 @@ function dw_theme_styles() {
   wp_enqueue_script( 'bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
   // Main CSS
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
+
+  // Google Font - Anton
+  wp_enqueue_style( 'anton_font', 'https://fonts.googleapis.com/css?family=Anton');  
+
+  // Google Font - Space Mono
+  wp_enqueue_style( 'space_font', 'https://fonts.googleapis.com/css?family=Space+Mono');  
 }
 
 add_action( 'wp_enqueue_scripts', 'dw_theme_styles' );

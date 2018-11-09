e6884d6d19420974789436f24034e22700ac6e38
Daniel McIntyre <dmcintyre25@gmail.com>
2018-11-09 08:48:42 -0500

    added wordpress loop and sample images

--- index.php
+++ index.php
@@ -11,21 +11,28 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card leadingCard mx-auto">
-                        <img class="img-fluid rounded" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>
+
+                        <?php
+                        $args = array(
+                            'posts_per_page' => 1
+                        );
+                        $query = new WP_Query( $args );
+                        ?>
+
+                        <?php if( $query->have_posts() ) : while($query->have_posts() ) : $query->the_post(); ?>
+
+                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large', array('class' => 'img-fluid rounded')); ?></a>
+                        <?php endwhile; else : ?>
+            
+                            <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
+        
+                        <?php endif; ?>
+                        
                     </div>
                 </div>
             </div>
 
-            
-            <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-            
-            <!-- Post Content Here -->
-
-            <?php endwhile; else : ?>
-            
-                <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
-            
-            <?php endif; ?>
+                  
         </div>
     </section>
 
@@ -57,126 +64,42 @@
 
 
 <div class="row mx-4">
-      
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
-                </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
-                </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+    $args = array(
+        'offset' => 1
+    );
+    $query = new WP_Query( $args );
+    ?>
 
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
-                </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
 
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
+        <div class="col-lg-4">
+            <div class="card childCard mx-auto">
+                <div class="cardImage">                
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small', array('class' => 'img-fluid')); ?></a>
                 </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
+                <div class="cardText">
+                    <h3 class="text-center cardTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <p><?php the_excerpt(); ?></p>
+                </div>
+                <div class="cardFooter">
+                    <div class="cardAuthor">
+                        <?php the_author_posts_link(); ?>
+                    </div>
+                    <div class="cardDate">
+                        <p><?php echo get_the_date(); ?></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
-                </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
+    <?php endif; ?>
 
-    <div class="col-lg-4">
-        <div class="card childCard mx-auto">
-            <div class="cardImage">                
-                <img class="img-fluid" src=<?php print(get_template_directory_uri() . '/featureImage.jpg'); ?>>                
-            </div>
-            <div class="cardText">
-                <h3 class="text-center cardTitle">Getting Started with React Hooks</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero turpis. In id ante nec metus semper maximus vel eu lorem. Aliquam erat volutpat.</p>
-            </div>
-            <div class="cardFooter">
-                <div class="cardAuthor">
-                    <a href="#">Daniel McIntyre</a>
-                </div>
-                <div class="cardDate">
-                    <p>November 7th, 2018</p>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
 
 <?php get_footer(); ?>

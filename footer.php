421540cbae808e500eb55b449d8763a6440dffcf
Daniel McIntyre <dmcintyre25@gmail.com>
2018-11-10 09:17:18 -0500

    adding owner section to footer

--- footer.php
+++ footer.php
@@ -5,15 +5,22 @@
 	<div class="row">
         <div class="col-lg-12">
 			<div class="row">
-				<div class="col-lg-8">
+				<div class="col-lg-7">
 					<p>
 					&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?>
 					</p>
 				</div>
 
-				<div class="col-lg-4">
-					<p>Brought to you by Daniel McIntyre.</p>
-					<p>Follow him on Twitter <a href="https://twitter.com/dmcintyre25">here.</a></p>
+				<div class="col-lg-5">	
+					<div class="owner">				
+						<div class="ownerText">
+							<p>Brought to you by Daniel McIntyre.</p>
+							<p>Follow him on Twitter <a href="https://twitter.com/dmcintyre25">here.</a></p>
+						</div>
+						<div class="ownerImage">					
+								<img class="img-circle img-fluid" src="/devworld/wp-content/themes/devworld/Images/danielHeadshot.jpg">											
+						</div>
+					</div>
 				</div>	
 			</div>
 		</div>	

e14f56f0168add406a64b351904e5cb6d5af4d95
Daniel McIntyre <dmcintyre25@gmail.com>
2018-11-17 09:59:07 -0500

    updated the page template

--- page.php
+++ page.php
@@ -1,20 +1,29 @@
 <?php get_header(); ?>
 
-<h1>DevWorld</h1>
-
-
 <section>
-    <div>
+    <div class="pageBanner text-center align-middle">
         <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
         
-            <h2><?php the_title(); ?></h2>
-            <p><?php the_excerpt(); ?></p>
+            <h1 class="pageTitle"><?php the_title(); ?></h1>
 
-        <?php endwhile; else : ?>
-        
-            <p><?php _e( 'Sorry, no pages matched your criteria.' ) ?></p>
-        
-        <?php endif; ?>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-2">
+        </div>
+        <div class="col-lg-8">
+            <div class="pageContent">
+                    <p><?php the_content(); ?></p>
+
+                <?php endwhile; else : ?>
+                
+                    <p><?php _e( 'Sorry, no pages matched your criteria.' ) ?></p>
+                
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="col-lg-2">
+        </div>
     </div>
 </section>
 

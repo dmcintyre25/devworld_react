7c2813484ba7ec1a1238bfb23db7fc1753838e60
Daniel McIntyre <dmcintyre25@gmail.com>
2019-01-25 08:25:53 -0500

    updated cards to flexbox card styling on front page

--- content-posts.php
+++ content-posts.php
@@ -1,44 +1,39 @@
-<div class="row mx-4">
+
     
     <?php
-
- 
-    $numPosts = ( is_front_page() ) ? 4 : -1;
-    $offset = ( is_front_page() ) ? 1 : 0;    
-    $cat = get_query_var('category_name');
-    
-    $args = array(
-        'offset' => $offset,
-        'posts_per_page' => $numPosts,
-        'category_name' => $cat
-    );
-    $query = new WP_Query( $args );
+        $numPosts = ( is_front_page() ) ? 4 : -1;
+        $offset = ( is_front_page() ) ? 1 : 0;    
+        $cat = get_query_var('category_name');
+        
+        $args = array(
+            'offset' => $offset,
+            'posts_per_page' => $numPosts,
+            'category_name' => $cat
+        );
+        $query = new WP_Query( $args );
     ?>
 
-    <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+    <div class="row mx-3">
+        <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+            
+                <div class="card  col-lg-4 col-md-12 px-0 my-2 ">
+                 
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small', array('class' => 'card-img-top p-0 img-fluid')); ?></a>
+                        <div class="card-body">
+                            <h3 class="text-center card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>                        
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                                                    
+                            <div class="d-flex justify-content-around align-items-end">
+                                <?php the_author_posts_link(); ?>                          
+                                <?php echo get_the_date(); ?>
+                            </div>
+                        </div>
 
-        <div class="col-lg-4 col-sm-6">
-            <div class="card childCard mx-auto">
-                <div class="cardImage">                
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small', array('class' => 'img-fluid')); ?></a>
                 </div>
-                <div class="cardText">
-                    <h3 class="text-center cardTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <p><?php the_excerpt(); ?></p>
-                </div>
-                <div class="cardFooter">
-                    <div class="cardAuthor">
-                        <?php the_author_posts_link(); ?>
-                    </div>
-                    <div class="cardDate">
-                        <p><?php echo get_the_date(); ?></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+            
+        <?php endwhile; else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
 
-    <?php endwhile; else : ?>
-        <p><?php _e( 'Sorry, no posts matched your criteria.' ) ?></p>
-    <?php endif; ?>
-
-</div>
+            
+        <?php endif; ?>
+    </div>
--- functions.php
+++ functions.php
@@ -57,7 +57,7 @@ add_theme_support( 'post-thumbnails' );
  * @return int (Maybe) modified excerpt length.
  */
 function wpdocs_custom_excerpt_length( $length ) {
-  return 20;
+  return 30;
 }
 
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
